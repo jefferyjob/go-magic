b534f8fa295cc3fdf5d135c00f19990d24870996
libin <libinjob@163.com>
2023-07-05 00:24:38 +0800

    dir init

--- internal/data/dao/users.go
+++ internal/data/dao/users.go
@@ -1,1 +1,19 @@
 package dao
+
+import (
+	"context"
+	"gorm.io/gorm"
+)
+
+// UsersDao 数据访问层
+type UsersDao struct {
+	Ctx context.Context
+	*gorm.DB
+}
+
+func NewUsersDao(ctx context.Context) *UsersDao {
+	return &UsersDao{
+		Ctx: ctx,
+		//DB:  resource.DefaultDb().WithContext(ctx),
+	}
+}
--- internal/data/entity/users.go
+++ internal/data/entity/users.go
@@ -1,1 +1,11 @@
 package entity
+
+// Users DB实体层
+type Users struct {
+	ID   int    `json:"id" gorm:"column:id"`
+	Name string `json:"name" gorm:"column:name"`
+}
+
+func (m *Users) TableName() string {
+	return "users"
+}
